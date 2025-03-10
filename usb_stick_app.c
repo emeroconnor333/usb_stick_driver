768a280f5d6ce9dc46db439ee5b55751c832ccd7
Conor Lydon <conor.lydon8@gmail.com>
2025-03-10 20:23:12 +0000

    Update usb_stick_app.c
    Added encryption and decryption using caesar cipher. Only works for lower case letters(a=<&&z>=) and upper case letters (A=<&&Z>=) to ignore special characters and spaces

--- usb_stick_app.c
+++ usb_stick_app.c
@@ -8,29 +8,56 @@
 #define BUFFER_SIZE 1024
 
 int fd;
+int shift; // Shift value for Caesar cipher
+
+// Function to encrypt text using Caesar cipher
+void encrypt(char *text, int shift) {
+    for (int i = 0; text[i] != '\0'; i++) {
+        if (text[i] >= 'A' && text[i] <= 'Z') {
+            text[i] = 'A' + (text[i] - 'A' + shift) % 26;
+        } else if (text[i] >= 'a' && text[i] <= 'z') {
+            text[i] = 'a' + (text[i] - 'a' + shift) % 26;
+        }
+    }
+}
+
+
+// Function to decrypt text using Caesar cipher
+void decrypt(char *text, int shift) {
+    for (int i = 0; text[i] != '\0'; i++) {
+        if (text[i] >= 'A' && text[i] <= 'Z') {
+            text[i] = 'A' + (text[i] - 'A' - shift + 26) % 26;
+        } else if (text[i] >= 'a' && text[i] <= 'z') {
+            text[i] = 'a' + (text[i] - 'a' - shift + 26) % 26;
+        }
+    }
+}
+
+
 
 void *write_thread(void *arg) {
     char write_buffer[BUFFER_SIZE];
     ssize_t bytes_written;
 
-  
-        // Prompt the user for input
-        printf("Enter data to write to the USB stick: ");
-        fgets(write_buffer, BUFFER_SIZE, stdin);  // Read user input
+    // Prompt the user for input
+    printf("Enter data to write to the USB stick: ");
+    fgets(write_buffer, BUFFER_SIZE, stdin);  // Read user input  
+    write_buffer[strcspn(write_buffer, "\n")] = '\0'; // Remove newline
 
-        // Remove newline character if it exists
-        write_buffer[strcspn(write_buffer, "\n")] = '\0';
+    // Encrypt before writing
+    encrypt(write_buffer, shift);
 
-        // Write data to the device
-        bytes_written = write(fd, write_buffer, strlen(write_buffer));
-        if (bytes_written == -1) {
-            perror("Failed to write to USB stick");
-            return NULL;
-        }
-
-        printf("Written %zd bytes: %s\n", bytes_written, write_buffer);
+    // Write data to the device
+    bytes_written = write(fd, write_buffer, strlen(write_buffer));
+    if (bytes_written == -1) {
+        perror("Failed to write to USB stick");
         return NULL;
     }
+
+    printf("Written %zd bytes (Encrypted): %s\n", bytes_written, write_buffer);
+    return NULL;
+}
+
 
 
 void *read_thread(void *arg) {
@@ -45,6 +72,11 @@ void *read_thread(void *arg) {
 
         // Null-terminate the read buffer
         read_buffer[bytes_read] = '\0';
+
+          // Decrypt after reading
+        decrypt(read_buffer, shift);
+
+    
         printf("Read %zd bytes: %s\n", bytes_read, read_buffer);
         return NULL;
     }
@@ -53,6 +85,18 @@ void *read_thread(void *arg) {
 int main() {
     pthread_t writer, reader;
     int choice;
+
+
+      // Ask the user for a shift value
+    printf("Enter the shift amount for the Caesar cipher: ");
+    if (scanf("%d", &shift) != 1) {
+        printf("Invalid input. Please enter a number.\n");
+        return 1;
+    }
+    getchar(); // Consume newline left in input buffer
+
+
+    
 
     // Open the USB stick device file
     fd = open("/dev/usb_stick", O_RDWR);
@@ -65,8 +109,8 @@ int main() {
 
        while (1) {
         printf("\nUSB Stick Menu:\n");
-        printf("1. Write to USB\n");
-        printf("2. Read from USB\n");
+        printf("1. Write to USB (Encrypted)\n");
+        printf("2. Read from USB(Decrypted)\n");
         printf("3. Exit\n");
         printf("Enter your choice: ");
 
